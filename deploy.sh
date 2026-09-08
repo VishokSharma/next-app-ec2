df396cbecd5ccdc0d362e46901a5109c2f01195c
Vishok Sharma <vishokmait@gmail.com>
2026-09-08 23:37:37 +0530

    Load Node environment during deployment

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,15 @@
+#!/bin/bash
+
+set -e
+
+# Load NVM so node and npm are available
+export NVM_DIR="$HOME/.nvm"
+source "$NVM_DIR/nvm.sh"
+
 cd ~/next-app-ec2
+
 git pull origin main
+
 npm install
+
 npm run build
-npm run start
